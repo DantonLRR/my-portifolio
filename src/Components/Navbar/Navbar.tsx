c40b26fd2c9cb8f375c10d010510a2ce85f90dd0
Danton Lucas <danyonlucasrr@gmail.com>
2025-02-14 17:00:56 -0300

    feat: creates styles button component and init navbar desktop component, create animated background

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import { AppBar, MenuItem, styled, Toolbar,  } from "@mui/material"
+
+const Navbar = () => {
+    const Styledtoolbar = styled(Toolbar)(({ }) => ({
+        display: "flex",
+        justifyContent: "space-evenly",
+    }))
+    return (
+        <>
+            <AppBar position="relative">
+                <Styledtoolbar>
+                    <MenuItem> About</MenuItem>
+                    <MenuItem> Skills</MenuItem>
+                    <MenuItem> Projects</MenuItem>
+                </Styledtoolbar>
+            </AppBar>
+
+        </>
+    )
+}
+
+export default Navbar
