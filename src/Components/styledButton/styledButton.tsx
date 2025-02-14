97f44a65e78ff8c126f32c9063a173123834e239
Danton Lucas <danyonlucasrr@gmail.com>
2025-02-14 20:28:21 -0300

    feat: adds onClick at styled

--- src/Components/styledButton/styledButton.tsx
+++ src/Components/styledButton/styledButton.tsx
@@ -2,8 +2,9 @@ import { styled } from "@mui/material"
 import { ReactNode } from "react"
 interface StyledButtonProps{
     children: ReactNode
+    onClick:()=> void
 }
-const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
+const StyledButton: React.FC<StyledButtonProps> = ({children, onClick}) => {
 
     const StyledButton = styled("button")(({theme}) => ({
         backgroundColor: "transparent",
@@ -23,7 +24,7 @@ const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
 
     return (
         <>
-            <StyledButton>
+            <StyledButton onClick={onClick}>
             {children}
             </StyledButton>
         </>
